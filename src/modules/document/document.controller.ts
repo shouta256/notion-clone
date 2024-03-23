8e80f6e0ac5f6b92800e6286b8bf6be26b2a1410
Shouta256 <shouta160618@gmail.com>
2024-03-23 15:08:41 +0900

    [update] アーカイブのAPIを追加
    アーカイブのドキュメントの取得と削除用APIを追加

--- src/modules/document/document.controller.ts
+++ src/modules/document/document.controller.ts
@@ -7,11 +7,13 @@ import {
   UseGuards,
   Request,
   Patch,
+  Delete,
 } from '@nestjs/common';
 import { DocumentService } from './document.service';
 import { Document } from 'src/entities/document.entity';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from '../auth/auth.service';
+import { DocumentDataDTO } from './documentDto/documentData.dto';
 
 @Controller('document')
 @UseGuards(AuthGuard('jwt'))
@@ -43,7 +45,7 @@ export class DocumentController {
   @Get('/')
   async getDocumentsByUserId(
     @Request() req: Request,
-  ): Promise<Document[] | number> {
+  ): Promise<DocumentDataDTO[] | number> {
     const userId = await this.authService.getUserIdFromAuthHeader(req);
     if (!userId) {
       return 401;
@@ -59,12 +61,19 @@ export class DocumentController {
     return this.documentService.getDocumentsByParentId(parentDocumentId);
   }
 
+  //アーカイブのドキュメントを取得
+  @Get('archive')
+  async getArchive(@Request() req: Request): Promise<Document[]> {
+    const userId = await this.authService.getUserIdFromAuthHeader(req);
+    return this.documentService.getArchive(userId);
+  }
+
   //ドキュメントとその子を全てアーカイブする
   @Patch('archive/:documentId')
   async moveToArchive(
     @Param('documentId') documentId: number,
     @Request() req: Request,
-  ): Promise<Document[]> {
+  ): Promise<DocumentDataDTO[]> {
     const userId = await this.authService.getUserIdFromAuthHeader(req);
 
     await this.documentService.moveToArchiveRecursive(documentId);
@@ -72,12 +81,17 @@ export class DocumentController {
     return await this.documentService.getDocumentsByUserId(userId);
   }
 
+  @Delete('archive/:documentId')
+  async deleteArchive(@Param('documentId') documentId: number) {
+    return await this.documentService.deleteArchive(documentId);
+  }
+
   //ドキュメントとその子を全てリストアする
   @Patch('restore/:documentId')
   async moveToRecover(
     @Param('documentId') documentId: number,
     @Request() req: Request,
-  ): Promise<Document[]> {
+  ): Promise<DocumentDataDTO[]> {
     const userId = await this.authService.getUserIdFromAuthHeader(req);
     await this.documentService.moveToRestoreRecursive(documentId);
 
