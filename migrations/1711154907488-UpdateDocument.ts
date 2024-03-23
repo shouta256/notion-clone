a11fe1ba5845867550f7ad8f39ccd55551a325b2
Shouta256 <shouta160618@gmail.com>
2024-03-23 10:25:30 +0900

    [fix] documentのバグ修正
    userIdが二重にできてしまったので、マイグレーションファイルを全て削除し生成し直した

--- migrations/1711154907488-UpdateDocument.ts
+++ migrations/1711154907488-UpdateDocument.ts
@@ -1,18 +1,20 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class DeleteDocumentUserId1711105605287 implements MigrationInterface {
-    name = 'DeleteDocumentUserId1711105605287'
+export class UpdateDocument1711154907488 implements MigrationInterface {
+    name = 'UpdateDocument1711154907488'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE \`document\` DROP COLUMN \`user_id\``);
         await queryRunner.query(`ALTER TABLE \`document\` DROP FOREIGN KEY \`FK_7424ddcbdf1e9b067669eb0d3fd\``);
-        await queryRunner.query(`ALTER TABLE \`document\` CHANGE \`userId\` \`userId\` int NULL`);
+        await queryRunner.query(`ALTER TABLE \`document\` CHANGE \`userId\` \`userId\` int NOT NULL DEFAULT '-1'`);
         await queryRunner.query(`ALTER TABLE \`document\` ADD CONSTRAINT \`FK_7424ddcbdf1e9b067669eb0d3fd\` FOREIGN KEY (\`userId\`) REFERENCES \`user\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE \`document\` DROP FOREIGN KEY \`FK_7424ddcbdf1e9b067669eb0d3fd\``);
-        await queryRunner.query(`ALTER TABLE \`document\` CHANGE \`userId\` \`userId\` int NOT NULL`);
+        await queryRunner.query(`ALTER TABLE \`document\` CHANGE \`userId\` \`userId\` int NULL`);
         await queryRunner.query(`ALTER TABLE \`document\` ADD CONSTRAINT \`FK_7424ddcbdf1e9b067669eb0d3fd\` FOREIGN KEY (\`userId\`) REFERENCES \`user\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE \`document\` ADD \`user_id\` int NOT NULL DEFAULT '-1'`);
     }
 
 }
--- migrations/1711155347229-UpdateDocument.ts
+++ migrations/1711155347229-UpdateDocument.ts
@@ -1,18 +1,18 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class AddDocumentUserId1711106329789 implements MigrationInterface {
-    name = 'AddDocumentUserId1711106329789'
+export class UpdateDocument1711155347229 implements MigrationInterface {
+    name = 'UpdateDocument1711155347229'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`document\` DROP FOREIGN KEY \`FK_7424ddcbdf1e9b067669eb0d3fd\``);
-        await queryRunner.query(`ALTER TABLE \`document\` CHANGE \`userId\` \`userId\` int NULL DEFAULT '-1'`);
+        await queryRunner.query(`DROP INDEX \`FK_7424ddcbdf1e9b067669eb0d3fd\` ON \`document\``);
+        await queryRunner.query(`ALTER TABLE \`document\` CHANGE \`userId\` \`userId\` int NOT NULL DEFAULT '-1'`);
         await queryRunner.query(`ALTER TABLE \`document\` ADD CONSTRAINT \`FK_7424ddcbdf1e9b067669eb0d3fd\` FOREIGN KEY (\`userId\`) REFERENCES \`user\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE \`document\` DROP FOREIGN KEY \`FK_7424ddcbdf1e9b067669eb0d3fd\``);
         await queryRunner.query(`ALTER TABLE \`document\` CHANGE \`userId\` \`userId\` int NULL`);
-        await queryRunner.query(`ALTER TABLE \`document\` ADD CONSTRAINT \`FK_7424ddcbdf1e9b067669eb0d3fd\` FOREIGN KEY (\`userId\`) REFERENCES \`user\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`CREATE INDEX \`FK_7424ddcbdf1e9b067669eb0d3fd\` ON \`document\` (\`userId\`)`);
     }
 
 }
