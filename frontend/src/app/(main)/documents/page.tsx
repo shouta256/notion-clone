8029d9577d02fe851fe51313fe54b7dd20fe2443
Shouta256 <shouta160618@gmail.com>
2024-03-26 15:21:45 +0900

    [add]ドキュメントページを作成

---
+++ frontend/src/app/(main)/documents/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import React, { useEffect, useState } from 'react';
+
+export default function DocumentsPage() {
+  return (
+    <div>
+      <h1>documentsページ</h1>
+    </div>
+  );
+}
