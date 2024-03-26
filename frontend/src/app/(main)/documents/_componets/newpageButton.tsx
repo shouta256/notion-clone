fe3d2c994fe036f39acfc6d2fb4e473613499c85
Shouta256 <shouta160618@gmail.com>
2024-03-27 08:59:59 +0900

    [add]新規ドキュメントを作成するボタンコンポーネントを作成

---
+++ frontend/src/app/(main)/documents/_componets/newpageButton.tsx
@@ -0,0 +1,34 @@
+import { Flex, Button, Icon } from '@chakra-ui/react';
+import { AddIcon } from '@chakra-ui/icons';
+import { createDocument } from '@/app/api';
+import { useMutation, useQueryClient } from 'react-query';
+
+export const NewpageButton = () => {
+  const queryClient = useQueryClient();
+  const mutation = useMutation(() => createDocument('Untitle'), {
+    onSuccess: () => {
+      queryClient.invalidateQueries('document');
+    },
+  });
+  const handleOnclick = async () => {
+    console.log('押された');
+    await mutation.mutateAsync();
+  };
+
+  return (
+    <Button
+      leftIcon={<Icon as={AddIcon} />}
+      paddingLeft='1'
+      paddingRight='2'
+      fontSize='md'
+      fontWeight='medium'
+      width='100%'
+      marginX='auto'
+      textAlign='left'
+      justifyContent='flex-start'
+      onClick={handleOnclick}
+    >
+      New page
+    </Button>
+  );
+};
