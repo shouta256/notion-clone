07e7971843012e76969ac9694727da86d948ef51
Shouta256 <shouta160618@gmail.com>
2024-03-27 08:59:04 +0900

    [add]ドキュメント用のlayoutページを作成

---
+++ frontend/src/app/(main)/documents/layout.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import { useAuth } from '@/app/hooks/useAuth';
+import { redirect } from 'next/navigation';
+import { Box, Flex } from '@chakra-ui/react';
+import Sidebar from './_componets/sidebar';
+
+export default function MainLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <Flex>
+      <Sidebar />
+
+      <Box ml='250px' p='4' flex='1'>
+        {children}
+      </Box>
+    </Flex>
+  );
+}
