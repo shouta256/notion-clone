c38d3b0cf6bdfc8b729a1155084f3af12b558b31
Shouta256 <shouta160618@gmail.com>
2024-03-29 07:51:22 +0900

    [add] ページがないとき用のページ
    ユーザがページを持っていないときに新規ページを作ることができる

--- frontend/src/app/(main)/documents/page.tsx
+++ frontend/src/app/(main)/documents/page.tsx
@@ -1,11 +1,42 @@
 'use client';
 
-import React, { useEffect, useState } from 'react';
+import { createDocument } from '@/app/api';
+import { AddIcon } from '@chakra-ui/icons';
+import { Box, Button, Center, Heading, Icon } from '@chakra-ui/react';
+import { useRouter } from 'next/navigation';
+import React from 'react';
+import { useMutation, useQueryClient } from 'react-query';
 
 export default function DocumentsPage() {
+  const router = useRouter();
+
+  const queryClient = useQueryClient();
+  const mutation = useMutation(() => createDocument('Untitle'), {
+    onSuccess: (data) => {
+      queryClient.invalidateQueries('documentList');
+      router.push(`/documents/${data.id}`);
+    },
+  });
+
+  const handleOnclick = async () => {
+    await mutation.mutateAsync();
+  };
+
   return (
-    <div>
-      <h1>documentsページ</h1>
-    </div>
+    <Center height='90vh'>
+      <Box textAlign='center'>
+        <Heading marginY='50px'>Welcome to Notion</Heading>
+        <Button
+          mt='4'
+          color='white'
+          backgroundColor='black'
+          leftIcon={<Icon as={AddIcon} />}
+          onClick={handleOnclick}
+          _hover={{ backgroundColor: 'gray.800' }}
+        >
+          Create a note
+        </Button>
+      </Box>
+    </Center>
   );
 }
