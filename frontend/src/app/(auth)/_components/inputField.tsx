56594c29e2e95b49485398c8df79947457baaf31
Shouta256 <shouta160618@gmail.com>
2024-03-29 09:38:42 +0900

    [update] パスワードを表示できるように変更

--- frontend/src/app/(auth)/_components/inputField.tsx
+++ frontend/src/app/(auth)/_components/inputField.tsx
@@ -1,5 +1,13 @@
-import { Box, FormControl, FormLabel, Input } from '@chakra-ui/react';
-import { ChangeEvent } from 'react';
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Button,
+} from '@chakra-ui/react';
+import { ChangeEvent, useState } from 'react';
+import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
 
 interface InputFieldProps {
   label: string;
@@ -9,28 +17,41 @@ interface InputFieldProps {
   placeholder: string;
 }
 
-const InputField = ({
+export const InputField = ({
   label,
   type,
   value,
   onChange,
   placeholder,
 }: InputFieldProps) => {
+  const [showPassword, setShowPassword] = useState(false);
+
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     onChange(e);
   };
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
     <FormControl>
       <FormLabel maxWidth='400px'>{label}</FormLabel>
-      <Input
-        type={type}
-        value={value}
-        onChange={handleChange}
-        placeholder={placeholder}
-        maxWidth='400px'
-      />
+      <InputGroup maxWidth='400px'>
+        <Input
+          type={showPassword ? 'text' : type}
+          value={value}
+          onChange={handleChange}
+          placeholder={placeholder}
+        />
+        {type === 'password' && (
+          <InputRightElement>
+            <Button onClick={togglePasswordVisibility} h='1.75rem' size='sm'>
+              {showPassword ? <ViewOffIcon /> : <ViewIcon />}
+            </Button>
+          </InputRightElement>
+        )}
+      </InputGroup>
     </FormControl>
   );
 };
-
-export default InputField;
