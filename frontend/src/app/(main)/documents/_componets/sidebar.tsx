5a1c5e3c152943d6c42e1cddef6828c7083739cd
shota <shouta160618@gmail.com>
2025-02-18 16:25:22 -0600

    [fix]Trashの使用をやめる

--- frontend/src/app/(main)/documents/_componets/sidebar.tsx
+++ frontend/src/app/(main)/documents/_componets/sidebar.tsx
@@ -1,65 +1,64 @@
-import { Box, VStack } from '@chakra-ui/react';
+import { Box, VStack } from "@chakra-ui/react";
 
-import { redirect } from 'next/navigation';
-import { useQuery } from 'react-query';
-import { useEffect, useState } from 'react';
+import { redirect } from "next/navigation";
+import { useQuery } from "react-query";
+import { useEffect, useState } from "react";
 
-import { getDocuments, getUser } from '@/app/api';
-import { NestedDocuments } from '@/app/type';
+import { getDocuments, getUser } from "@/app/api";
+import { NestedDocuments } from "@/app/type";
 
-import { NewpageButton } from './newpageButton';
-import { DocumentList } from './documentList';
-import { Profile } from './profile';
-import TrashBox from './trashBox';
+import { NewpageButton } from "./newpageButton";
+import { DocumentList } from "./documentList";
+import { Profile } from "./profile";
 
 //ユーザ名、ドキュメントの階層を表示するコンポーネント
 export const Sidebar = () => {
-  const [token, setToken] = useState('');
+	const [token, setToken] = useState("");
 
-  //トークンからユーザを取得
-  const userQueryResult = useQuery('user', () => getUser(token), {
-    enabled: !!token,
-  });
-  const { data: user, isLoading } = userQueryResult;
+	//トークンからユーザを取得
+	const userQueryResult = useQuery("user", () => getUser(token), {
+		enabled: !!token,
+	});
+	const { data: user, isLoading } = userQueryResult;
 
-  //ドキュメントを取得
-  const documentsQueryResult = useQuery<NestedDocuments[] | undefined>(
-    'documentList',
-    () => getDocuments()
-  );
-  const { data: documents } = documentsQueryResult;
+	//ドキュメントを取得
+	const documentsQueryResult = useQuery<NestedDocuments[] | undefined>(
+		"documentList",
+		() => getDocuments(),
+	);
+	const { data: documents } = documentsQueryResult;
 
-  useEffect(() => {
-    const storedToken = localStorage.getItem('token');
-    if (!storedToken) {
-      redirect('/');
-    }
-    setToken(storedToken);
-  }, []);
+	useEffect(() => {
+		const storedToken = localStorage.getItem("token");
+		if (!storedToken) {
+			redirect("/");
+		}
+		setToken(storedToken);
+	}, []);
 
-  return (
-    <Box
-      as='nav'
-      bg='gray.100'
-      color='black'
-      width='250px'
-      height='100vh'
-      p='2'
-      position='fixed'
-      top='0'
-      left='0'
-      overflowY='auto'
-      maxHeight='100vh'
-    >
-      <VStack spacing='4' align='stretch'>
-        {user && user.userName && !isLoading && (
-          <Profile userName={user.userName} />
-        )}
+	return (
+		<Box
+			as="nav"
+			bg="gray.100"
+			color="black"
+			width="250px"
+			height="100vh"
+			p="2"
+			position="fixed"
+			top="0"
+			left="0"
+			overflowY="auto"
+			maxHeight="100vh"
+		>
+			<VStack spacing="4" align="stretch">
+				{user && user.userName && !isLoading && (
+					<Profile userName={user.userName} />
+				)}
 
-        <NewpageButton />
-        {documents?.length !== 0 && <DocumentList documents={documents!} />}
-        <TrashBox />
-      </VStack>
-    </Box>
-  );
+				<NewpageButton />
+				{documents?.length !== 0 && <DocumentList documents={documents!} />}
+				{/* <TrashBox /> */}
+			</VStack>
+		</Box>
+	);
 };
