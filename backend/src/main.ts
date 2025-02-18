bff6158f9c916e3f9aa58dd670052e79927f6ff7
shota <shouta160618@gmail.com>
2025-02-18 17:26:44 -0600

    fix02

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,15 +1,17 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { CorsOptions } from "@nestjs/common/interfaces/external/cors-options.interface";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const corsOptions: CorsOptions = {
-    origin: true,
-    credentials: true,
-    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-  };
-  app.enableCors(corsOptions);
-  await app.listen(8080);
+	const app = await NestFactory.create(AppModule);
+	const corsOptions: CorsOptions = {
+		origin: "https://notion-clone-ten-delta.vercel.app", // 許可するオリジンを指定
+		credentials: true,
+		methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE", "OPTIONS"],
+	};
+	app.enableCors(corsOptions);
+
+	const port = process.env.PORT || 8080;
+	await app.listen(port);
 }
 bootstrap();
